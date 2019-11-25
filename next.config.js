8d28a75be2988ab9041912b08edd58a029e60189
Kevin Tovar <kevintovar@esimez.mx>
2019-11-24 21:52:13 -0600

    Move outside withOffline

--- next.config.js
+++ next.config.js
@@ -2,8 +2,10 @@ const withPlugins = require('next-compose-plugins')
 const optimizedImages = require('next-optimized-images')
 const withOffline = require('next-offline')
 
-exports.default = withPlugins([[optimizedImages, {}], withOffline], {
-  env: {
-    GRAPHQL_ENDPOINT: process.env.GRAPHQL_ENDPOINT,
-  },
-})
+exports.default = withOffline(
+  withPlugins([[optimizedImages, {}]], {
+    env: {
+      GRAPHQL_ENDPOINT: process.env.GRAPHQL_ENDPOINT,
+    },
+  })
+)
